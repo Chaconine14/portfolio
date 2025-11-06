f118de02a9ad54162ded738c42c7b942cdd42b10
宮内沙英 <sae@miyauchisaenoMacBook-Air.local>
2025-11-06 22:47:24 +0900

    アニメーションまで完成

--- main.js
+++ main.js
@@ -37,34 +37,75 @@ window.addEventListener('resize', () => {
 });
 
 
+
+//============================================
 //GSAPアニメーション
-// gsap.to(".p-logo", {
+//============================================
+gsap.registerPlugin(ScrollTrigger);
 
 
-//     y: () => window.innerHeight,
-//     scrollTrigger: {
-//         toggleActions: "play none none reverse",
-//         trigger: "body", // アニメーションが始まるトリガーとなる要素
-//         start: "top top", //→ trigger要素の中心が画面下部に来たら開始  
-//         end: "bottom bottom", // → trigger要素の下端が画面下端に来たら終了
-//         scrub: true, // スクロール量に合わせてアニメーション
-//         pin: true, // 要素を固定
-//         markers: true,
+// クジラのロゴをゆらゆら揺らすアニメーション
+gsap.fromTo(
+    ".js-whale, .js-whale2",
+    {
+        y: 0,
+        rotation: -20,       // 傾ける角度（時計回り＝左端が下がる）
+        ease: "sine.inOut",// 滑らかな上下動
+        repeat: -1,        // 永遠に繰り返す
+        yoyo: true,        // 行ったり来たりを繰り返す
+    },
+    {
+        y: -50,
+        rotation: 10,
+        ease: "sine.inOut",
+        repeat: -1,        // 永遠に繰り返す
+        yoyo: true,
+        duration: 1.8,     // 1往復にかかる時間（波のテンポ）
+    }
+);
 
-//     },
-// });
+
+
+// 左下へ見切れる
+const tl1 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".js-whale",
+        start: "bottom center", // トップエリア30%で開始
+        toggleActions: "play none none reverse",
+        // scrub: true,
+
+    }
+});
+tl1.to(".js-whale", {
+    x: "-60vw",      // ゆったり左下へ
+    y: "100vh",
+    duration: 4,     // ゆっくり泳ぐ速度
+    ease: "sine.inOut",
+});
 
 
 
-// gsap.to(".p-logo", {
-//     x: "80vw",
-//     y: "100vh",
-//     scrollTrigger: {
-//         trigger: "l-main__bg",
-//         start: "top top",
-//         end: "bottom bottom",
-//         scrub: true,
-//         markers: true,
+// 右下でフェードインして停止（揺れは継続）
+const tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".js-skills",
+        start: "top top",
+        toggleActions: "play none none reverse",
+    }
+});
+tl2.fromTo(
+    ".js-whale2",
+    {
+        x: "1vw",   // ← 画面の右の外からスタート
+        opacity: 0,
+    },
+    {
+        x: "0",       // ← 元の位置（右下）に戻る
+        opacity: 1,   // ← ふわっと表示
+        duration: 0.5,
+        ease: "power2.out", // ← やわらかくフェードイン    
+    }
+);
 
-//     }
-// });
+
+
