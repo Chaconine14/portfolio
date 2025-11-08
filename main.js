9ba027b30b4b1d521d44461e59ea6ac690e2b77c
宮内沙英 <sae@miyauchisaenoMacBook-Air.local>
2025-11-08 11:14:06 +0900

    リファクタリング前

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ gsap.fromTo(
         ease: "sine.inOut",
         repeat: -1,        // 永遠に繰り返す
         yoyo: true,
-        duration: 1.8,     // 1往復にかかる時間（波のテンポ）
+        duration: 2.2,     // 1往復にかかる時間（波のテンポ）
     }
 );
 
@@ -72,15 +72,13 @@ const tl1 = gsap.timeline({
         trigger: ".js-whale",
         start: "bottom center", // トップエリア30%で開始
         toggleActions: "play none none reverse",
-        // scrub: true,
+        scrub: 4,
 
     }
 });
 tl1.to(".js-whale", {
-    x: "-60vw",      // ゆったり左下へ
-    y: "100vh",
-    duration: 4,     // ゆっくり泳ぐ速度
-    ease: "sine.inOut",
+    x: "-65vw",      // ゆったり左下へ
+    y: "50vh",
 });
 
 
@@ -109,3 +107,15 @@ tl2.fromTo(
 
 
 
+
+// クジラロゴ（右下）クリックでスムーススクロール
+const whaleLogo = document.querySelector('.js-whale2');
+
+whaleLogo.addEventListener('click', (e) => {
+    e.preventDefault(); // ページの即リロードを防ぐ
+
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // スムーズにスクロール！
+    });
+});
